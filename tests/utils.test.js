b01be4154a979ea49769c4aafc19b3ea66b0e6fe
LWRGitHub <LoganReynoldsLWR@gmail.com>
2021-02-03 19:46:35 -0800

    DONE

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -3,11 +3,6 @@ const chai = require("chai")
 const utils = require("../utils")
 const expect = chai.expect
 
-// ========================================================
-// NOTE: https://mochajs.org/#arrow-functions
-// Passing arrow functions (“lambdas”) to Mocha is discouraged.
-// Lambdas lexically bind this and cannot access the Mocha context.
-// ========================================================
 
 it("should say hello", function() {
   const hello = utils.sayHello()
@@ -17,24 +12,36 @@ it("should say hello", function() {
 })
 
 // ========================================================
-// Level 1 Challenges
-// 1. Write the pending tests check that they are pending, like this:
+// 1. Tests check that they are pending, like this:
 //    it("should do something that you want done")
-// 2. Next, write the test and see that it fails.
-// 3. Write the code in the utils.js file to make the test pass.
-// 4. Finally see if you would like to refactor your code at all.
-// This is called "Red-Green-Refactor"
+// 2. Code writen in the utils.js file to make the test pass.
 // ========================================================
 
+// ========================================================
+// Calculations
+// ========================================================
 
+it("should return the area of a rectangle", function() {
+  const area = utils.area(2,4)
+  expect(area).to.be.a("number")
+  expect(area).to.equal(8)
+})
+
+it("should return the perimeter of a rectangle", function() {
+  const perimeter = utils.perimeter(1,1)
+  expect(perimeter).to.be.a("number")
+  expect(perimeter).to.equal(4)
+})
+
+it("Should return the area of a circle with radius", function() {
+  const circleArea = utils.circleArea(2)
+  expect(circleArea).to.be.a("number")
+  expect(circleArea).to.equal(12.566370614359172)
+})
 
 
 // ========================================================
-// Level 2 Challenges
-// ========================================================
-// NOTE: The following unimplemented test cases are examples
-// of "Pending Tests" in Chai. Someone should write these
-// tests eventually.
+// Shopping Cart
 // ========================================================
 
 beforeEach((done) => {
@@ -50,20 +57,26 @@ it("Should create a new (object) Item with name and price", function() {
   expect(item).to.have.property("quantity", 1)
 })
 
-it("Should return an array containing all items in cart")
+it("Should return an array containing all items in cart", function(){
+  const getShoppingCart = utils.getShoppingCart()
+  expect(getShoppingCart).to.be.an('array')
+})
 
-it("Should add a new item to the shopping cart")
+it("Should add a new item to the shopping cart", function(){
+  const preCount = utils.getNumItemsInCart()
+  const addItemToCart = utils.addItemToCart({name:"Some Name",price:2,quantity:1})
+  expect(utils.getNumItemsInCart()).is.greaterThan(preCount)
+})
 
-it("Should return the number of items in the cart")
+it("Should return the number of items in the cart", function(){
+  const getNumItemsInCart = utils.getNumItemsInCart()
+  expect(getNumItemsInCart).to.be.a("number")
+})
 
-it("Should remove items from cart")
+it("Should remove items from cart", function(){
+  const addItemToCart = utils.addItemToCart("Some Item")
+  const preCount = utils.getNumItemsInCart()
+  const removeItemFromCart = utils.removeItemFromCart("Some Item")
+  expect(utils.getNumItemsInCart()).is.lessThan(preCount)
+})
 
-// ========================================================
-// Stretch Challenges
-// ========================================================
-
-it("Should update the count of items in the cart")
-
-it("Should validate that an empty cart has 0 items")
-
-it("Should return the total cost of all items in the cart")
--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 // ========================================================
-// Level 1 Challenges
+// Calculations
 // ========================================================
 
 const sayHello = () => {
@@ -7,22 +7,20 @@ const sayHello = () => {
 }
 
 const area = (w, h) => {
-  // should return the area
+  return w*h;
 }
 
 const perimeter = (w, h) => {
-  // should return the perimeter
+  return (w+h)*2;
 }
 
 const circleArea = r => {
-  // should return the area of the circle
+  return Math.PI*(r*r)
 }
 
+
 // ========================================================
-// Level 2 Challenges
-// ========================================================
-// NOTE: You will need to implement methods below (not yet
-// defined) in order to make the tests pass.
+// Shopping Cart
 // ========================================================
 
 const shoppingCart = []
@@ -36,19 +34,19 @@ const createItem = (name, price) => {
 }
 
 const getShoppingCart = () => {
-  // should return the current state of shopping cart
+  return shoppingCart
 }
 
 const addItemToCart = (item) => {
-  // should add item to shopping cart
+  shoppingCart.push(item)
 }
 
 const getNumItemsInCart = () => {
-  // should return the total quantity of items in cart
+  return shoppingCart.length
 }
 
 const removeItemFromCart = (item) => {
-  // should remove item from shopping cart
+  shoppingCart.pop(item);
 }
 
 module.exports = {
